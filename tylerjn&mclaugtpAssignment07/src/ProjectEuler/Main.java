89d7152efcdb2a4f1f4f373a43b1fe06c9fdb235
unknown <tylerjn@IL-PC-144.uccc.uc.edu>
2015-03-03 16:56:07 -0500

    Heres the code for the main

--- tylerjn&mclaugtpAssignment07/src/ProjectEuler/Main.java
+++ tylerjn&mclaugtpAssignment07/src/ProjectEuler/Main.java
@@ -3,8 +3,15 @@ package ProjectEuler;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Problem06 p = new Problem06();
+		
+		System.out.println("Project Euler");
+		System.out.println("Problem 06");
+		System.out.println("Sum of Squares = " + p.sumOfSquares(10));
+		System.out.println("Square of Sum = " + p.squaresOfSum(10));
+		System.out.println("The answer of the difference: " + p.squaresOfSum(10) + " - " + p.sumOfSquares(10) + " = " + (p.squaresOfSum(10) - p.sumOfSquares(10)));
+		
 	}
 
 }
